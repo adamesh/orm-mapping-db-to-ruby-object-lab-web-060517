ae45c4d4f0738e481dcc9ad84e9a7a27b94fb88d
adamesh <adameshh@gmail.com>
2017-06-15 10:47:11 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,45 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT id, name, grade FROM students;
+    SQL
+    self.execute_sql_and_return_object_array(sql)
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE name == ?;
+    SQL
+    student_stats = DB[:conn].execute(sql, name).first
+    self.new_from_db(student_stats)
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -39,5 +55,53 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE grade = 9;
+    SQL
+    self.execute_sql_and_return_object_array(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE grade < 12;
+    SQL
+    self.execute_sql_and_return_object_array(sql)
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE grade = 10 LIMIT ?;
+    SQL
+    self.execute_sql_and_return_object_array(sql, x)
+  end
+
+  #takes in a sql string, executes it, then returns array of resulting objects
+  def self.execute_sql_and_return_object_array(sql, *args)
+    students_array = DB[:conn].execute(sql, *args)
+    students_array.map do |student|
+      self.new_from_db(student)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE grade = 10 LIMIT 1;
+    SQL
+    self.execute_sql_and_return_object_array(sql).first
+  end
+
+  def self.all_students_in_grade_x(x)
+    sql = <<-SQL
+      SELECT id, name, grade FROM students
+      WHERE grade = ?;
+    SQL
+    self.execute_sql_and_return_object_array(sql, x)
   end
 end
